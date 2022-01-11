d67e9ff65a54a2512c640c2210efe4bf9bcacdae
Roman <sparrowroman@yandex.ru>
2022-01-11 22:21:59 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,47 @@
+public class HomeWorkApp {
+    public static void main(String[] agrs) {
+
+        printThreeWords();
+        checkSumSign(1, 5);
+        printColor(1000);
+        compareNumbers(50,20);
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSign(int a, int b) {
+        /*int a = 1; // первый способ
+        int b = 5;*/
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
